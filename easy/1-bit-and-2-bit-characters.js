325e80c3bd96c03066230fc7b77f1aeb80e7a8f9
Dorin <d.dumitrascuta@gmail.com>
2020-06-13 21:37:09 +0100

    Add the solution! First problem solved on leetCode

--- easy/1-bit-and-2-bit-characters.js
+++ easy/1-bit-and-2-bit-characters.js
@@ -8,18 +8,24 @@
  */
 
 const isOneBitCharacter = bits => {
-	// if only 1 bit then true
-	if (bits.length == 1) return true;
+	let l = bits.length;
+	let isBit = false;
 
-	// [1,0,0], [1,1,0]
-	// look at bit before last
-	if (bits[bits.length - 2] === 1) return false;
+	while (bits.length > 0) {
+		for (let i = 0; i < bits.length; i++) {
+			if (bits[i] === 0 && i === 0) {
+				bits.splice(i, 1);
+				isBit = true;
+				break;
+			}
 
-	return true;
+			if ((bits[i] === 1 && bits[i + 1] === 0) || bits[i + 1] === 1) {
+				bits.splice(i, 2);
+				if (bits.length === 0) isBit = false;
+				break;
+			}
+		}
+	}
 
-	// ok so can't do this imperatively,
-	// the sequence of bits [] I think has random test cases up to 1,000 length
-	if (bits.length > 3) return false;
-
-	// need a decoder function
+	return isBit;
 };
