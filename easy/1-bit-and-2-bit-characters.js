5c4c733efedd54f70eb9228f22bd50ce9e4cdf82
Dorin <d.dumitrascuta@gmail.com>
2020-06-13 13:45:15 +0100

    Need to figure out a general rule as the string of bits can be 1,000 len

--- easy/1-bit-and-2-bit-characters.js
+++ easy/1-bit-and-2-bit-characters.js
@@ -7,4 +7,19 @@
  * @return {boolean}
  */
 
-const isOneBitCharacter = bits => {};
+const isOneBitCharacter = bits => {
+	// if only 1 bit then true
+	if (bits.length == 1) return true;
+
+	// [1,0,0], [1,1,0]
+	// look at bit before last
+	if (bits[bits.length - 2] === 1) return false;
+
+	return true;
+
+	// ok so can't do this imperatively,
+	// the sequence of bits [] I think has random test cases up to 1,000 length
+	if (bits.length > 3) return false;
+
+	// need a decoder function
+};
